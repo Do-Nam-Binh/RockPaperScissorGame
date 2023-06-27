744ab34360f88db5907710a5bea93da70934ceb4
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-06-27 15:58:44 +0700

    Add function to determine winner

--- game.js
+++ game.js
@@ -11,3 +11,19 @@ function getComputerChoice() {
     }
 }
 
+function playGame(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection){
+        return ("Draw! Both played " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)));
+    }
+    if(playerSelection === "rock" && computerSelection === "paper" ||playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock" ){
+        return ("You Lose! ") + (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)) + " beats " + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
+    }
+
+    if(playerSelection === "rock" && computerSelection === "scissors" ||playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper" ){
+        return ("You Win! ") + (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)) + " beats " +  (computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1));
+    }
+}
+
